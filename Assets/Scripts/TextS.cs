ceecf751e1740572e426746f6f1fc295c2056b90
Just <justtardis@gmail.com>
2018-03-05 18:34:16 +1000

    Start work

---
+++ Assets/Scripts/TextS.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text>().text = "КЕЙС\n\"СМАРТФОНЫ\"";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
