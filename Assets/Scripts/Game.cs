2b060ec174147014a1f88148e5679aa09ce455d8
Elmina <ElminaRoss@gmail.com>
2018-03-07 15:46:38 +1000

    cases

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public float silver; // серебро
+    public float gold; // золото
+    public Case[] cases; //кейсы
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
+
+//Класс кейса
+[System.Serializable]
+public class Case
+{
+    public int id; 
+    public string name; // имя
+    public float price; // цена
+    public Item[] items;
+    public Sprite picture; //картинка
+}
+
+//Класс кейса
+[System.Serializable]
+public class Item
+{
+    public int id;
+    public string name; // имя
+    public float price; // цена
+    public Sprite picture; //картинка
+}
