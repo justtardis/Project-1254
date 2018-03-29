3171cc0faff2d3364a6b8de5233cf6f23c89beb1
Elmina <ElminaRoss@gmail.com>
2018-03-29 16:43:33 +1000

    data

---
+++ Assets/Scripts/DataLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour {
+
+    public string siteName = "http://casesim/data.php";
+    public string res;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        WWW userData = new WWW(siteName);
+        yield return userData;
+        res = userData.text;
+        Debug.Log(res);
+    }
+	
+
+}
